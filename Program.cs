416dde9f6f35770c446d7f2c53e879cfe75a2e85
Stanislav <stankhil@gmail.com>
2025-01-15 08:50:00 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace _14._01
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int MessageBox(IntPtr hWnd, String text, String caption, int type);
+
+        static void Main()
+        {
+            MessageBox(IntPtr.Zero, "Мене звати Хіль Станіслав", "Name", 0);
+            MessageBox(IntPtr.Zero, "Я навчаюся программування", "Course", 0);
+            MessageBox(IntPtr.Zero, "Мені 17 років", "Age", 0);
+            MessageBox(IntPtr.Zero, "Мій номер телефону: 380 99 999 99 99", "Phone", 0);
+            
+        }
+    }
+}
